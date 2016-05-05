732cf16a56365aa3f3de1cdf720933a58ab48496
Emma C Whitaker <ewhitaker17@my.whitworth.edu>
2016-05-04 19:17:36 -0700

    changes....

--- IntelligenceCheck/IntelligenceCheck/ViewController.swift
+++ IntelligenceCheck/IntelligenceCheck/ViewController.swift
@@ -12,16 +12,52 @@ class CharacterSheetViewController: UIViewController, UITextFieldDelegate, UIIma
 
     // MARK: Properties
     @IBOutlet weak var nameEntry: UITextField!
+    @IBOutlet weak var levelEntry: UITextField!
+    @IBOutlet weak var profEntry: UITextField!
+    @IBOutlet weak var strEntry: UITextField!
+    @IBOutlet weak var dexEntry: UITextField!
+    @IBOutlet weak var conEntry: UITextField!
+    @IBOutlet weak var intEntry: UITextField!
+    @IBOutlet weak var wisEntry: UITextField!
+    @IBOutlet weak var chaEntry: UITextField!
+    @IBOutlet weak var strMod: UILabel!
+    @IBOutlet weak var dexMod: UILabel!
+    @IBOutlet weak var conMod: UILabel!
+    @IBOutlet weak var intMod: UILabel!
+    @IBOutlet weak var wisMod: UILabel!
+    @IBOutlet weak var chaMod: UILabel!
 
     @IBOutlet weak var nameField: UILabel!
-        
+    
+    var level = 0
+    var prof = 2
+    var str = 8
+    var dex = 8
+    var con = 8
+    var wis = 8
+    var int = 8
+    var cha = 8
     var characterName: String? = "default"
+    var strValue = ""
+    var dexValue = ""
+    var conValue = ""
+    var intValue = ""
+    var wisValue = ""
+    var chaValue = ""
     var player : Player = Player()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Handle the text field's user input through delegate callbacks
         nameEntry.delegate = self
+        levelEntry.delegate = self
+        profEntry.delegate = self
+        strEntry.delegate = self
+        dexEntry.delegate = self
+        conEntry.delegate = self
+        intEntry.delegate = self
+        wisEntry.delegate = self
+        chaEntry.delegate = self
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -64,6 +100,60 @@ class CharacterSheetViewController: UIViewController, UITextFieldDelegate, UIIma
         nameEntry.text = textField.text
         characterName = nameEntry.text
         nameField.text = nameEntry.text
+        if (str == 8){
+            strValue = "-1"
+            strMod.text = strValue
+        }
+        if (str == 9){
+            strValue = "-1"
+            strMod.text = strValue
+        }
+        if (str == 10){
+            strValue = "0"
+            strMod.text = strValue
+        }
+        if (str == 11){
+            strValue = "0"
+            strMod.text = strValue
+        }
+        if (str == 12){
+            strValue = "+1"
+            strMod.text = strValue
+        }
+        if (str == 13){
+            strValue = "+1"
+            strMod.text = strValue
+        }
+        if (str == 14){
+            strValue = "+2"
+            strMod.text = strValue
+        }
+        if (str == 15){
+            strValue = "+2"
+            strMod.text = strValue
+        }
+        if (str == 16){
+            strValue = "+3"
+            strMod.text = strValue
+        }
+        if (str == 17){
+            strValue = "+3"
+            strMod.text = strValue
+        }
+        if (str == 18){
+            strValue = "+4"
+            strMod.text = strValue
+        }
+        if (str == 19){
+            strValue = "+4"
+            strMod.text = strValue
+        }
+        if (str == 20){
+            strValue = "+5"
+            strMod.text = strValue
+        }
+        
+        
     }
 
     // MARK: Actions
